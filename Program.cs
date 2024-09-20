91ead6d7fde5c627e6288f7532209d866b83f16d
MarkusBercel <markusb@kkszki.hu>
2024-09-20 13:08:52 +0200

    The result is ready

--- Program.cs
+++ Program.cs
@@ -8,8 +8,37 @@ namespace terulet
 {
     internal class Program
     {
+        static void terulet()
+        {
+            Console.WriteLine("Téglalap területének számítása.");
+            int a;
+            int b;
+            int T;
+
+            do
+            {
+
+                Console.WriteLine("Kérem 'a' oldal értékét (0> kell lennie)");
+                a = int.Parse(Console.ReadLine());
+            } while (a<=0);
+
+            do
+            {
+                Console.WriteLine("Kérem 'b' oldal értékét (0> kell lennie)");
+                b = int.Parse(Console.ReadLine());
+            } while (b <= 0);
+
+            
+            T = a * b;
+            Console.WriteLine($"A téglalap területe: {T}");
+            
+
+        }
+
         static void Main(string[] args)
         {
+            terulet();  
+
         }
     }
 }
